dea361a9929f8f0559f5227d2bfbc7f8de82b207
MuhammadKhairunnasNurdin <anasnurdin936@gmail.com>
2024-06-28 01:25:32 +0700

    Feat(Setup dependencies): add forgotten file in commit before

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+// export default {
+module.exports = {
+  content: [
+      "./resources/views/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('flowbite/plugin'),
+      require('tailwindcss/plugin')(function ({addBase}) {
+          addBase({
+              '[type="search"]::-webkit-search-decoration': {display: 'none'},
+              '[type="search"]::-webkit-search-cancel-button': {display: 'none'},
+              '[type="search"]::-webkit-search-results-button': {display: 'none'},
+              '[type="search"]::-webkit-search-results-decoration': {display: 'none'},
+          })
+      }),
+  ],
+}
+
